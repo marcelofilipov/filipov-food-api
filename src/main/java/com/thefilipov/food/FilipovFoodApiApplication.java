a71925deacadf5e71a5f58ed9f218093b2d03332
Marcelo Filipov <marcelo.filipov@gmail.com.br>
2019-11-14 10:29:36 -0300

    The first commit

---
+++ src/main/java/com/thefilipov/food/FilipovFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.thefilipov.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilipovFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilipovFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thefilipov/food/FilipovFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefilipov.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilipovFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
