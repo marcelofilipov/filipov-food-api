877f38bae9652164550eebdd321ca84cbfecbbab
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-11-16 22:33:09 -0200

    3.12. Excluindo um objeto do banco de dados

--- src/main/java/com/thefilipov/food/jpa/CadastroCozinha.java
+++ src/main/java/com/thefilipov/food/jpa/CadastroCozinha.java
@@ -31,5 +31,11 @@ public class CadastroCozinha {
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
+	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
 
 }
---
+++ src/main/java/com/thefilipov/food/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.thefilipov.food.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.thefilipov.food.FilipovFoodApiApplication;
+import com.thefilipov.food.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(FilipovFoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		
+		cadastroCozinha.remover(cozinha);
+	}
+
+}
