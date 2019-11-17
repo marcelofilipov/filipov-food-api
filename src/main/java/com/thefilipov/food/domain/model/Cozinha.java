06850965e639755a3cadb96a208024eacb78a6b3
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-11-16 22:04:44 -0200

    3.9. Adicionando um objeto no banco de dados

--- src/main/java/com/thefilipov/food/domain/model/Cozinha.java
+++ src/main/java/com/thefilipov/food/domain/model/Cozinha.java
@@ -1,11 +1,9 @@
 package com.thefilipov.food.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Cozinha {
--- src/main/java/com/thefilipov/food/jpa/CadastroCozinha.java
+++ src/main/java/com/thefilipov/food/jpa/CadastroCozinha.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thefilipov.food.domain.model.Cozinha;
 
@@ -20,6 +21,11 @@ public class CadastroCozinha {
 		TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
 		
 		return query.getResultList();
+	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
 	}
 
 }
