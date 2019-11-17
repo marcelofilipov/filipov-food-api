87133c47c120bdc12d255e6be40b97646f5218ae
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-11-16 22:20:48 -0200

    3.11. Atualizando um objeto no banco de dados

--- src/main/java/com/thefilipov/food/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/thefilipov/food/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.thefilipov.food.FilipovFoodApiApplication;
 import com.thefilipov.food.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(FilipovFoodApiApplication.class)
@@ -16,18 +16,13 @@ public class InclusaoCozinhaMain {
 
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		cozinha.setNome("Brasileira");
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
+		cozinha = cadastroCozinha.salvar(cozinha);
 		
-		cozinha1 = cadastroCozinha.salvar(cozinha1);
-		cozinha2 = cadastroCozinha.salvar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
-		
+		System.out.printf("%d - %s\n", cozinha.getId(), cozinha.getNome());
 	}
 
 }
