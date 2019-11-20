819984717d7240029dc794c630ae721c2b668bd3
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-11-20 18:53:28 -0200

    4.10. Modelando e requisitando um Collection Resource com GET

---
+++ src/main/java/com/thefilipov/food/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.thefilipov.food.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thefilipov.food.domain.model.Cozinha;
+import com.thefilipov.food.domain.model.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.todas();
+	}
+	
+}
