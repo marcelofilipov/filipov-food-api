52f1664f5e8d73ad132843c104827d4f60188471
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-05-20 19:49:48 -0300

    Modeling and implementing resource updates with PUT

--- src/main/java/com/thefilipov/food/api/controller/CozinhaController.java
+++ src/main/java/com/thefilipov/food/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.thefilipov.food.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,19 +44,28 @@ public class CozinhaController {
 		if (cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
-		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		
 		return ResponseEntity.notFound().build();
-		
-		//HttpHeaders headers = new HttpHeaders();
-		//headers.add(HttpHeaders.LOCATION,"http://localhost:8082/foodapi/cozinhas");
-		
-		//return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.porId(cozinhaId);
+		
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
