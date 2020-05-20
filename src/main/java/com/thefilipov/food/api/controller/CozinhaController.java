0c78a18c38e13c416c5ec60f5fc2a243d8433281
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-05-20 18:37:18 -0300

    Modeling and implementing the inclusion of features with POST

--- src/main/java/com/thefilipov/food/api/controller/CozinhaController.java
+++ src/main/java/com/thefilipov/food/api/controller/CozinhaController.java
@@ -3,13 +3,15 @@ package com.thefilipov.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thefilipov.food.api.model.CozinhasXmlWrapper;
@@ -37,8 +39,11 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
 		
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
 		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		return ResponseEntity.ok(cozinha);
+		return ResponseEntity.notFound().build();
 		
 		//HttpHeaders headers = new HttpHeaders();
 		//headers.add(HttpHeaders.LOCATION,"http://localhost:8082/foodapi/cozinhas");
@@ -46,4 +51,10 @@ public class CozinhaController {
 		//return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+
 }
