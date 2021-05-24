9d1462e383e2749e9478141f09528b5d1669c070
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-24 20:56:21 -0300

    Generating the API documentation in HTML with Swagger UI and SpringFox

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -1,17 +1,25 @@
 package com.thefilipov.food.core.openapi;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                     .apis(RequestHandlerSelectors.any())
                     .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("index.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
