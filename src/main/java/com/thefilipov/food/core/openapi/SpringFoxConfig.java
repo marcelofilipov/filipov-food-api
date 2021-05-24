57b7cce75cbaeee2733d4a43a82272acc0cb5ca6
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-24 17:20:51 -0300

    Dependency Updated

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SpringFoxConfig {
 
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                     .apis(RequestHandlerSelectors.any())
                     .build();
