1f23911af596a7cfcdc1cbc6ab185f5ea0c7e3d0
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-24 21:07:52 -0300

    Selecting the API endpoints to generate the documentation

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.thefilipov.food.core.openapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +11,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SpringFoxConfig implements WebMvcConfigurer {
 
+    @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                    .apis(RequestHandlerSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("com.thefilipov.food.controller"))
                     .build();
     }
 
