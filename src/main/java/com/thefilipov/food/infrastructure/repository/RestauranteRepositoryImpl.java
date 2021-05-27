b1c2903d552c7d3d5d9280916274083ccda72dcf
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-27 20:44:36 -0300

    Adding restrictions to the where clause with Criteria API

--- src/main/java/com/thefilipov/food/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/thefilipov/food/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -24,7 +26,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryCustomize
         CriteriaBuilder builder = manager.getCriteriaBuilder();
 
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-        criteria.from(Restaurante.class);
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
