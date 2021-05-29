ac4e441b5d4eb9bb99222c99ba01cf9f9c3f367a
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-29 19:20:22 -0300

    Mapping many-to-many relationships with @ManyToMany

--- src/main/java/com/thefilipov/food/domain/model/Restaurante.java
+++ src/main/java/com/thefilipov/food/domain/model/Restaurante.java
@@ -1,14 +1,10 @@
 package com.thefilipov.food.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -32,5 +28,11 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+		joinColumns = @JoinColumn(name = "restaurante_id"),
+		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
