e5702d7d8597856afc74f7729fc6544173818baa
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-05-30 17:02:46 -0300

    Testing and analyzing the impact of class embedding in the REST API

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,17 +3,6 @@ insert into cozinha (id, nome) values (2, 'Indiana');
 insert into cozinha (id, nome) values (3, 'Argentina');
 insert into cozinha (id, nome) values (4, 'Brasileira');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Bangkok Gourmet', 11.90, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Nova Delhi Delivery', 24.75, 2);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Sabor de Nova Delhi', 9.65, 2);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Paulistano Restaurante', 1.99, 4);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Churrasco Argentino', 0, 3);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Carne de Vaca Indiano', 0, 2);
-
-insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
-insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
-insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
-
 insert into estado (id, nome) values (1, 'Minas Gerais');
 insert into estado (id, nome) values (2, 'São Paulo');
 insert into estado (id, nome) values (3, 'Mato Grosso');
@@ -24,7 +13,18 @@ insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
 insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
-insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
+insert into cidade (id, nome, estado_id) values (5, 'Manaus', 3);
+
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Nova Delhi Delivery', 24.75, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Sabor de Nova Delhi', 9.65, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Paulistano Restaurante', 1.99, 4);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Churrasco Argentino', 0, 3);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Carne de Vaca Indiano', 0, 2);
+
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
