9e868d849b4f4cf0cfa60f7072ccd3a64da8b6e8
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-06-03 17:35:24 -0300

    Creating migrations and mapping the Pedido and ItemPedido entities

---
+++ src/main/java/com/thefilipov/food/domain/model/Pedido.java
@@ -0,0 +1,67 @@
+package com.thefilipov.food.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sub_total", nullable = false)
+    private BigDecimal subTotal;
+
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
+
+    @Column(name = "valor_total", nullable = false)
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCriacao;
+
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataConfirmacao;
+
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataCancelamento;
+
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataEntrega;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+}
