90c4033ae052e0910437466892498fc6c2dee2e0
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-06-23 13:11:26 -0300

    Customizing ResponseEntityExceptionHandler exception handlers

--- src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import com.thefilipov.food.domain.exception.NegocioException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -72,6 +73,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     private Problem.ProblemBuilder createProblemBuilder(
--- src/main/java/com/thefilipov/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/ProblemType.java
@@ -7,7 +7,8 @@ public enum ProblemType {
 
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
-    ERRO_NEGOCIO("/erro-negocio","Violação de regra de negócio");
+    ERRO_NEGOCIO("/erro-negocio","Violação de regra de negócio"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensível");
 
     private String title;
     private String uri;
