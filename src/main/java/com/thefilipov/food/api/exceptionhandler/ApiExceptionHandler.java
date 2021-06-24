d0b1a982a8b266009d66f5a92b2b9b75975d5cdc
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-06-24 09:24:14 -0300

    Handling other uncaught exceptions

--- src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -25,6 +25,21 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
+        String detail = "Ocorreu um erro interno inesperado no sistema. "
+                + "Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+
+        ex.printStackTrace();
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradoException(
             EntidadeNaoEncontradaException e, WebRequest request) {
--- src/main/java/com/thefilipov/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/ProblemType.java
@@ -9,7 +9,8 @@ public enum ProblemType {
     ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio","Violação de regra de negócio"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensível"),
-    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
 
     private String title;
     private String uri;
