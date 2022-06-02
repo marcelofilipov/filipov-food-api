691325dd83a35915372bd37dac50b343158b97c0
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-06-02 17:42:45 -0300

    Adding Endereço in the Restaurante resource representation model

--- src/main/java/com/thefilipov/food/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/thefilipov/food/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.thefilipov.food.api.model.EnderecoDTO;
+import com.thefilipov.food.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		var enderecoToEnderecoDTOTypeMap = modelMapper.createTypeMap(
+			Endereco.class, EnderecoDTO.class);
+		enderecoToEnderecoDTOTypeMap.<String>addMapping(
+			enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+			(enderecoDTODest, value) -> enderecoDTODest.getCidade().setEstado(value));
+			
+		return modelMapper;
 	}
 
 }
