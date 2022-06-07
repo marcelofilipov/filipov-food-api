3d90fefc13ffe9d73dcb3e9d4ed57e03d320eb53
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-06-07 09:01:37 -0300

    Implementation and adaptations in the tests of the Restaurante entity

---
+++ src/test/java/com/thefilipov/food/domain/service/CadastroRestauranteServiceTest.java
@@ -0,0 +1,10 @@
+package com.thefilipov.food.domain.service;
+
+import org.junit.jupiter.api.DisplayName;
+
+import com.thefilipov.food.ApplicationConfigTest;
+
+@DisplayName("CadastroRestauranteServiceTest")
+public class CadastroRestauranteServiceTest extends ApplicationConfigTest {
+
+}
