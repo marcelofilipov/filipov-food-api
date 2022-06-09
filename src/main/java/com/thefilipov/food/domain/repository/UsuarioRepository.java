9dee5ad9283044128e29050ff038e1f07d406c86
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-06-08 22:43:18 -0300

    Implementing business rule to avoid users with duplicate emails

--- src/main/java/com/thefilipov/food/domain/repository/UsuarioRepository.java
+++ src/main/java/com/thefilipov/food/domain/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.thefilipov.food.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.thefilipov.food.domain.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+	Optional<Usuario> findByEmail(String email);
 
 }
--- src/main/java/com/thefilipov/food/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/thefilipov/food/domain/service/CadastroUsuarioService.java
@@ -1,5 +1,7 @@
 package com.thefilipov.food.domain.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,15 @@ public class CadastroUsuarioService {
 	
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
+		usuarioRepository.detach(usuario);
+		
+		Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+		
+		if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+			throw new NegocioException(
+				String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+		}
+		
 		return usuarioRepository.save(usuario);
 	}
 	
