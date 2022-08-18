6acf481ae98caffb469722ba1a5bcf68e3a102ca
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-08-17 22:11:54 -0300

    Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/thefilipov/food/api/controller/PedidoController.java
+++ src/main/java/com/thefilipov/food/api/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package com.thefilipov.food.api.controller;
 
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.thefilipov.food.api.assembler.PedidoInput;
 import com.thefilipov.food.api.assembler.PedidoInputDisassembler;
 import com.thefilipov.food.api.assembler.PedidoModelAssembler;
@@ -12,8 +14,10 @@ import com.thefilipov.food.domain.model.Pedido;
 import com.thefilipov.food.domain.model.Usuario;
 import com.thefilipov.food.domain.repository.PedidoRepository;
 import com.thefilipov.food.domain.service.EmissaoPedidoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -39,11 +43,32 @@ public class PedidoController {
     private PedidoInputDisassembler pedidoInputDisassembler;
 
     @GetMapping
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue((pedidosModel));
+
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if (StringUtils.isNotBlank(campos)) {
+            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
+    }
+
+/*
+    @GetMapping
     public List<PedidoResumoModel> listar() {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
 
         return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
     }
+*/
 
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscar(@PathVariable String codigoPedido) {
--- src/main/java/com/thefilipov/food/api/model/PedidoResumoModel.java
+++ src/main/java/com/thefilipov/food/api/model/PedidoResumoModel.java
@@ -1,11 +1,13 @@
 package com.thefilipov.food.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@JsonFilter("pedidoFilter")
 @Getter
 @Setter
 public class PedidoResumoModel {
