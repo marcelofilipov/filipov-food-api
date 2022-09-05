d366e613149eafc9dd23af606c25e5be4c0edcf4
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-09-04 21:56:06 -0300

    Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -19,6 +19,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -42,6 +43,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
