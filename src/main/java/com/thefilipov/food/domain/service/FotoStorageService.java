06171c3e4b36ed942afdb236312ec3273a6a5b6d
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-11-27 14:26:22 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ src/main/java/com/thefilipov/food/domain/service/FotoStorageService.java
@@ -0,0 +1,21 @@
+package com.thefilipov.food.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+
+    @Builder
+    @Getter
+    class NovaFoto {
+
+        private String nomeArquivo;
+        private InputStream inputStream;
+
+    }
+
+}
---
+++ src/main/java/com/thefilipov/food/infrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,33 @@
+package com.thefilipov.food.infrastructure.service.storage;
+
+import com.thefilipov.food.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class LocalFotoStorageService implements FotoStorageService {
+
+    @Value("${filipov-food.storage.local.diretorio-fotos}")
+    private Path diretorioFotos;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+        try {
+            Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+
+            FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível armazenar arquivo.", e);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFotos.resolve(Path.of(nomeArquivo));
+
+    }
+
+}
