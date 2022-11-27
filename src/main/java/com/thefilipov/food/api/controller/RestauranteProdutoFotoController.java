26f7a4fc0cb91d351d93ad93c504a3875e116b0f
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-11-27 18:21:33 -0300

    Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/thefilipov/food/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/thefilipov/food/api/controller/RestauranteProdutoFotoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -31,7 +32,8 @@ public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
-                                          @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+                                          @PathVariable Long produtoId,
+                                          @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
         Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
         MultipartFile arquivo = fotoProdutoInput.getArquivo();
@@ -43,7 +45,7 @@ public class RestauranteProdutoFotoController {
         foto.setTamanho(arquivo.getSize());
         foto.setNomeArquivo(arquivo.getOriginalFilename());
 
-        FotoProduto fotoSalva = catalogoFotoProduto.salvar(foto);
+        FotoProduto fotoSalva = catalogoFotoProduto.salvar(foto, arquivo.getInputStream());
 
         return fotoProdutoModelAssembler.toModel(fotoSalva);
     }
--- src/main/java/com/thefilipov/food/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/thefilipov/food/domain/service/CatalogoFotoProdutoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 @Service
@@ -14,17 +15,32 @@ public class CatalogoFotoProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private FotoStorageService fotoStorage;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto) {
+    public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
+        String nomeNovoArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
 
         Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restauranteId, produtoId);
         if (fotoExistente.isPresent()) {
             produtoRepository.delete(fotoExistente.get());
         }
 
-        return produtoRepository.save(foto);
+        foto.setNomeArquivo(nomeNovoArquivo);
+        foto = produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        FotoStorageService.NovaFoto novaFoto = FotoStorageService.NovaFoto.builder()
+                .nomeArquivo(foto.getNomeArquivo())
+                .inputStream(dadosArquivo)
+                .build();
+
+        fotoStorage.armazenar(novaFoto);
+
+        return foto;
     }
 
 }
