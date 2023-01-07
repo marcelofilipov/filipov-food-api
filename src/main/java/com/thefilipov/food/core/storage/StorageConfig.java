596cf7de07c26672af9f0ddd65fe92b52ad6ccc4
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-01-07 14:51:20 -0300

    Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/thefilipov/food/core/storage/StorageConfig.java
+++ src/main/java/com/thefilipov/food/core/storage/StorageConfig.java
@@ -4,16 +4,21 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.thefilipov.food.domain.service.FotoStorageService;
+import com.thefilipov.food.infrastructure.service.storage.LocalFotoStorageService;
+import com.thefilipov.food.infrastructure.service.storage.S3FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AmazonS3Config {
+public class StorageConfig {
 
     @Autowired
     private StorageProperties storageProperties;
     @Bean
+    @ConditionalOnProperty(name = "filipov-food.storage.tipo", havingValue = "s3")
     public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(
                 storageProperties.getS3().getIdChaveAcesso(), storageProperties.getS3().getChaveAcessoSecreta());
@@ -22,5 +27,14 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(storageProperties.getS3().getRegiao())
                 .build();
+    }
+
+    @Bean
+    public FotoStorageService fotoStorageService() {
+        if (StorageProperties.TipoStorage.S3.equals(storageProperties.getTipo())) {
+            return new S3FotoStorageService();
+        } else {
+            return new LocalFotoStorageService();
+        }
     }
 }
