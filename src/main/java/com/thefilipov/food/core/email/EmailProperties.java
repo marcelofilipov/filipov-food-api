eda78a6d28a7ef6fe3d86759083dbd837d00dce1
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-01-14 19:03:01 -0300

    Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/thefilipov/food/core/email/EmailProperties.java
@@ -0,0 +1,21 @@
+package com.thefilipov.food.core.email;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("filipov-food.email")
+public class EmailProperties {
+
+    @NotNull
+    private  String remetente;
+
+}
---
+++ src/main/java/com/thefilipov/food/domain/service/EnvioEmailService.java
@@ -0,0 +1,20 @@
+package com.thefilipov.food.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Set;
+
+public interface EnvioEmailService {
+
+    void enviar(Mensagem mensagem);
+
+    @Getter
+    @Builder
+    class Mensagem {
+        private Set<String> destinatarios;
+        private String assunto;
+        private String corpo;
+
+    }
+}
