96ee5a104661a0af3abb4e766969ffde442a1909
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-02-13 18:47:07 -0300

    Reagindo a Domain Events em fases específicas da transação

--- src/main/java/com/thefilipov/food/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/thefilipov/food/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -3,9 +3,11 @@ package com.thefilipov.food.domain.listener;
 import com.thefilipov.food.domain.event.PedidoConfirmadoEvent;
 import com.thefilipov.food.domain.model.Pedido;
 import com.thefilipov.food.domain.service.EnvioEmailService;
+import com.thefilipov.food.domain.service.EnvioEmailService.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
@@ -13,11 +15,11 @@ public class NotificacaoClientePedidoConfirmadoListener {
     @Autowired
     private EnvioEmailService envioEmail;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
         Pedido pedido = event.getPedido();
 
-        var mensagem = EnvioEmailService.Mensagem.builder()
+        var mensagem = Mensagem.builder()
                 .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
                 .corpo("pedido-confirmado.html")
                 .variavel("pedido", pedido)
