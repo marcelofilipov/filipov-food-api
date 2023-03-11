737c3364e28dabce9ad558fedbec91b2a9e50339
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-11 10:15:49 -0300

    Habilitando o cache com o cabeçalho Cache-Control e a diretiva max-age

--- src/main/java/com/thefilipov/food/api/controller/FormaPagamentoController.java
+++ src/main/java/com/thefilipov/food/api/controller/FormaPagamentoController.java
@@ -1,21 +1,5 @@
 package com.thefilipov.food.api.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.thefilipov.food.api.assembler.FormaPagamentoInputDisassembler;
 import com.thefilipov.food.api.assembler.FormaPagamentoModelAssembler;
 import com.thefilipov.food.api.model.FormaPagamentoModel;
@@ -23,6 +7,15 @@ import com.thefilipov.food.api.model.input.FormaPagamentoInput;
 import com.thefilipov.food.domain.model.FormaPagamento;
 import com.thefilipov.food.domain.repository.FormaPagamentoRepository;
 import com.thefilipov.food.domain.service.CadastroFormaPagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
@@ -41,10 +34,14 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
 
     @GetMapping
-    public List<FormaPagamentoModel> listar() {
+    public ResponseEntity<List<FormaPagamentoModel>> listar() {
         List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
 
-        return formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+        List<FormaPagamentoModel> formasPagamentosModel = formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .body(formasPagamentosModel);
     }
     
     @GetMapping("/{formaPagamentoId}")
