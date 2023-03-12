c27e045f364f882dbeff565b32312a195da540de
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-12 14:48:12 -0300

    Entendendo e preparando a implementação de Deep ETags

--- src/main/java/com/thefilipov/food/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/thefilipov/food/domain/repository/FormaPagamentoRepository.java
@@ -2,9 +2,15 @@ package com.thefilipov.food.domain.repository;
 
 import com.thefilipov.food.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    OffsetDateTime getDataUltimaAtualização();
 
 }
