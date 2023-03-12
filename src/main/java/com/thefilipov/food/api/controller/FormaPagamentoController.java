ce0a71b6435df405b73d439321cf934c96ed0853
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-12 13:30:33 -0300

    Adicionando outras diretivas de Cache-Control na resposta HTTP

--- src/main/java/com/thefilipov/food/api/controller/FormaPagamentoController.java
+++ src/main/java/com/thefilipov/food/api/controller/FormaPagamentoController.java
@@ -40,7 +40,11 @@ public class FormaPagamentoController {
         List<FormaPagamentoModel> formasPagamentosModel = formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
 
         return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                //.cacheControl(CacheControl.maxAge(15, TimeUnit.SECONDS))
+                //.cacheControl(CacheControl.maxAge(15, TimeUnit.SECONDS).cachePrivate())
+                .cacheControl(CacheControl.maxAge(15, TimeUnit.SECONDS).cachePublic())
+                //.cacheControl(CacheControl.noCache())
+                //.cacheControl(CacheControl.noStore())
                 .body(formasPagamentosModel);
     }
     
