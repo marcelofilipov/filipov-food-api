4cb3cffdcbe23f197cce0229f7e346468104a5b7
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-19 14:00:31 -0300

    Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -21,7 +22,8 @@ public class SpringFoxConfig {
                     .paths(PathSelectors.any())
                     //.paths(PathSelectors.ant("/cozinhas/*"))
                     .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
     public ApiInfo apiInfo() {
