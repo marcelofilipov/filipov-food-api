21c2830389fae8efcb7f7493b877ed3d1bf1866d
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-19 13:55:23 -0300

    Descrevendo informações da API na documentação

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.thefilipov.food.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,7 +20,18 @@ public class SpringFoxConfig {
                     .apis(RequestHandlerSelectors.basePackage("com.thefilipov.food"))
                     .paths(PathSelectors.any())
                     //.paths(PathSelectors.ant("/cozinhas/*"))
-                    .build();
+                    .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Filipov Food API")
+                .description("API pública para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("Marcelo Filipov", null, "marcelo@filipov.dev.br"))
+                .build();
+
     }
 
 }
