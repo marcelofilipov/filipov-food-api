20c04b7059aebd7b88e0078dc129b682920f6f04
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-19 12:34:49 -0300

    Selecionando os endpoints da API para gerar a documentação

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package com.thefilipov.food.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,7 +14,9 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                    .apis(RequestHandlerSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("com.thefilipov.food"))
+                    .paths(PathSelectors.any())
+                    //.paths(PathSelectors.ant("/cozinhas/*"))
                     .build();
     }
 
