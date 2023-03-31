278bb2a48d978caf3f20de021af6f3b0ff3eed47
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-31 20:25:34 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -3,15 +3,22 @@ package com.thefilipov.food.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -25,11 +32,26 @@ public class SpringFoxConfig {
                     .paths(PathSelectors.any())
                     //.paths(PathSelectors.ant("/cozinhas/*"))
                     .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
-    public ApiInfo apiInfo() {
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Filipov Food API")
                 .description("API pública para clientes e restaurantes")
