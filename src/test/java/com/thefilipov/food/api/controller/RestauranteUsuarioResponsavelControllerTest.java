e3764bff50cb350b635a452d8f0fa9ee2dbd42c7
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-03-31 22:14:52 -0300

    Implementando e ajustes de testes unitários

--- src/test/java/com/thefilipov/food/api/controller/RestauranteUsuarioResponsavelControllerTest.java
+++ src/test/java/com/thefilipov/food/api/controller/RestauranteUsuarioResponsavelControllerTest.java
@@ -1,9 +1,9 @@
 package com.thefilipov.food.api.controller;
 
-import static org.mockito.Mockito.doNothing;
-
 import com.thefilipov.food.api.assembler.UsuarioModelAssembler;
+import com.thefilipov.food.domain.model.*;
 import com.thefilipov.food.domain.service.CadastroRestauranteService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -16,6 +16,18 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {RestauranteUsuarioResponsavelController.class})
 @ExtendWith(SpringExtension.class)
@@ -47,7 +59,7 @@ class RestauranteUsuarioResponsavelControllerTest {
      * Method under test: {@link RestauranteUsuarioResponsavelController#associar(Long, Long)}
      */
     @Test
-    void testAssociar2() throws Exception {
+    void testAssociarWithEncoding() throws Exception {
         doNothing().when(cadastroRestauranteService).associarResponsavel(Mockito.<Long>any(), Mockito.<Long>any());
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
                 .put("/restaurantes/{restauranteId}/responsaveis/{usuarioId}", 1L, 1L);
@@ -56,6 +68,88 @@ class RestauranteUsuarioResponsavelControllerTest {
                 .build()
                 .perform(requestBuilder);
         actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link RestauranteUsuarioResponsavelController#desassociar(Long, Long)}
+     */
+    @Test
+    void testDesassociar() throws Exception {
+        doNothing().when(cadastroRestauranteService).desassociarResponsavel(Mockito.<Long>any(), Mockito.<Long>any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/restaurantes/{restauranteId}/responsaveis/{usuarioId}", 1L, 1L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(restauranteUsuarioResponsavelController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link RestauranteUsuarioResponsavelController#desassociar(Long, Long)}
+     */
+    @Test
+    void testDesassociarWithEncoding() throws Exception {
+        doNothing().when(cadastroRestauranteService).desassociarResponsavel(Mockito.<Long>any(), Mockito.<Long>any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/restaurantes/{restauranteId}/responsaveis/{usuarioId}", 1L, 1L);
+        requestBuilder.characterEncoding("Encoding");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(restauranteUsuarioResponsavelController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link RestauranteUsuarioResponsavelController#listar(Long)}
+     */
+    @Test
+    @DisplayName("Retornar success(200) - Quando buscar 1 restaurante")
+    void getListarByIdAPI_RetornarStatus200() throws Exception {
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome(UsuarioControllerTest.NOME);
+        cozinha.setRestaurantes(new ArrayList<>());
+
+        Estado estado = new Estado();
+        estado.setId(1L);
+        estado.setNome(UsuarioControllerTest.NOME);
+
+        Cidade cidade = new Cidade();
+        cidade.setEstado(estado);
+        cidade.setId(1L);
+        cidade.setNome(UsuarioControllerTest.NOME);
+
+        Endereco endereco = new Endereco();
+        endereco.setBairro("Bairro");
+        endereco.setCep("Cep");
+        endereco.setCidade(cidade);
+        endereco.setComplemento("alice.liddell@example.org");
+        endereco.setLogradouro("Logradouro");
+        endereco.setNumero("Numero");
+
+        Restaurante restaurante = new Restaurante();
+        restaurante.setAberto(true);
+        restaurante.setAtivo(true);
+        restaurante.setCozinha(cozinha);
+        restaurante.setDataAtualizacao(OffsetDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.MIDNIGHT, ZoneOffset.UTC));
+        restaurante.setDataCadastro(OffsetDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.MIDNIGHT, ZoneOffset.UTC));
+        restaurante.setEndereco(endereco);
+        restaurante.setFormasPagamento(new HashSet<>());
+        restaurante.setId(1L);
+        restaurante.setNome(UsuarioControllerTest.NOME);
+        restaurante.setProdutos(new ArrayList<>());
+        restaurante.setResponsaveis(new HashSet<>());
+        restaurante.setTaxaFrete(BigDecimal.valueOf(42L));
+        when(cadastroRestauranteService.buscarOuFalhar(Mockito.<Long>any())).thenReturn(restaurante);
+        when(usuarioModelAssembler.toCollectionModel(Mockito.<Collection<Usuario>>any())).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/restaurantes/{restauranteId}/responsaveis", 1L);
+        MockMvcBuilders.standaloneSetup(restauranteUsuarioResponsavelController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/xml;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string("<List/>"));
     }
 }
 
--- src/test/java/com/thefilipov/food/api/controller/UsuarioGrupoControllerTest.java
+++ src/test/java/com/thefilipov/food/api/controller/UsuarioGrupoControllerTest.java
@@ -1,9 +1,10 @@
 package com.thefilipov.food.api.controller;
 
-import static org.mockito.Mockito.doNothing;
-
 import com.thefilipov.food.api.assembler.GrupoModelAssembler;
+import com.thefilipov.food.domain.model.Grupo;
+import com.thefilipov.food.domain.model.Usuario;
 import com.thefilipov.food.domain.service.CadastroUsuarioService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -16,6 +17,17 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {UsuarioGrupoController.class})
 @ExtendWith(SpringExtension.class)
@@ -47,7 +59,7 @@ class UsuarioGrupoControllerTest {
      * Method under test: {@link UsuarioGrupoController#associar(Long, Long)}
      */
     @Test
-    void testAssociar2() throws Exception {
+    void testAssociarWithEncoding() throws Exception {
         doNothing().when(cadastroUsuarioService).associarGrupo(Mockito.<Long>any(), Mockito.<Long>any());
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
                 .put("/usuarios/{usuarioId}/grupos/{grupoId}", 1L, 1L);
@@ -56,6 +68,59 @@ class UsuarioGrupoControllerTest {
                 .build()
                 .perform(requestBuilder);
         actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link UsuarioGrupoController#desassociar(Long, Long)}
+     */
+    @Test
+    void testDesassociar() throws Exception {
+        doNothing().when(cadastroUsuarioService).desassociarGrupo(Mockito.<Long>any(), Mockito.<Long>any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/usuarios/{usuarioId}/grupos/{grupoId}", 1L, 1L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(usuarioGrupoController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link UsuarioGrupoController#desassociar(Long, Long)}
+     */
+    @Test
+    void testDesassociarWithEncoding() throws Exception {
+        doNothing().when(cadastroUsuarioService).desassociarGrupo(Mockito.<Long>any(), Mockito.<Long>any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/usuarios/{usuarioId}/grupos/{grupoId}", 1L, 1L);
+        requestBuilder.characterEncoding("Encoding");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(usuarioGrupoController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Method under test: {@link UsuarioGrupoController#listar(Long)}
+     */
+    @Test
+    @DisplayName("Retornar success(200) - Quando buscar 1 grupo")
+    void getListarByIdAPI_RetornarStatus200() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setDataCadastro(OffsetDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.MIDNIGHT, ZoneOffset.UTC));
+        usuario.setEmail(UsuarioControllerTest.EMAIL);
+        usuario.setGrupos(new HashSet<>());
+        usuario.setId(1L);
+        usuario.setNome(UsuarioControllerTest.NOME);
+        usuario.setSenha(UsuarioControllerTest.SENHA);
+        when(cadastroUsuarioService.buscarOuFalhar(Mockito.<Long>any())).thenReturn(usuario);
+        when(grupoModelAssembler.toCollectionModel(Mockito.<Collection<Grupo>>any())).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/usuarios/{usuarioId}/grupos", 1L);
+        MockMvcBuilders.standaloneSetup(usuarioGrupoController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/xml;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string("<List/>"));
     }
 }
 
