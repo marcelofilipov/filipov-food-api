73b25eb1fce1067333ad2a765e101e7cc0cbcbe4
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-04-07 17:59:30 -0300

    Referenciando modelo de representação de problema com códigos de status de erro

--- src/main/java/com/thefilipov/food/api/exceptionhandler/Problem.java
+++ src/main/java/com/thefilipov/food/api/exceptionhandler/Problem.java
@@ -41,7 +41,7 @@ public class Problem {
     @Builder
     public static class Object {
 
-        @ApiModelProperty(example = "preço")
+        @ApiModelProperty(example = "preco")
         private String name;
 
         @ApiModelProperty(example = "O preço é obrigatório")
--- src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/thefilipov/food/core/openapi/SpringFoxConfig.java
@@ -8,11 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
@@ -23,6 +21,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -63,6 +62,8 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description(INTERNAL_SERVER_ERROR)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -76,10 +77,14 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description(BAD_REQUEST)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description(INTERNAL_SERVER_ERROR)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -88,6 +93,8 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
                         .description(UNSUPPORTED_MEDIA_TYPE)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build()
         );
     }
@@ -97,12 +104,22 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description(BAD_REQUEST)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description(INTERNAL_SERVER_ERROR)
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build()
         );
+    }
+
+    private Consumer<RepresentationBuilder> getProblemaModelReference() {
+        return r -> r.model(m -> m.name("Problema")
+                .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+                        q -> q.name("Problema").namespace("com.thefilipov.food.api.exceptionhandler")))));
     }
 
     private ApiInfo apiInfo() {
@@ -112,7 +129,6 @@ public class SpringFoxConfig {
                 .version("1")
                 .contact(new Contact("Marcelo Filipov", null, "marcelo@filipov.dev.br"))
                 .build();
-
     }
 
 }
