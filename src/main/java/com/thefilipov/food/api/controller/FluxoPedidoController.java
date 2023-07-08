16e732679d2ab07f7d0e9c359dc02d1e2018e625
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-07-08 18:35:24 -0300

    Descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/thefilipov/food/api/controller/FluxoPedidoController.java
+++ src/main/java/com/thefilipov/food/api/controller/FluxoPedidoController.java
@@ -1,13 +1,17 @@
 package com.thefilipov.food.api.controller;
 
+import com.thefilipov.food.api.openapi.controller.FluxoPedidoControllerDocumentation;
 import com.thefilipov.food.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = FluxoPedidoController.URI, produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerDocumentation {
+
+    public static final String URI = "/pedidos/{codigoPedido}";
 
     @Autowired
     private FluxoPedidoService fluxoPedido;
