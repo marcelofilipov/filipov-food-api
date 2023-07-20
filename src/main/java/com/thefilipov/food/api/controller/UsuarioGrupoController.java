8353afb54c554a9b79bb3399cc547fcf6465fcb9
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-07-20 18:18:32 -0300

    Descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/thefilipov/food/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/thefilipov/food/api/controller/UsuarioGrupoController.java
@@ -2,17 +2,21 @@ package com.thefilipov.food.api.controller;
 
 import com.thefilipov.food.api.assembler.GrupoModelAssembler;
 import com.thefilipov.food.api.model.GrupoModel;
+import com.thefilipov.food.api.openapi.controller.UsuarioGrupoControllerDocumentation;
 import com.thefilipov.food.domain.model.Usuario;
 import com.thefilipov.food.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = UsuarioGrupoController.URI, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerDocumentation {
+
+    protected static final String URI = "/usuarios/{usuarioId}/grupos";
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
